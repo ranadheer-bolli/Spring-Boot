9785871a180ddd923fe96ec3323a9889937c920c
ranadheer-bolli <ranadheer.bolli@zemosolabs.com>
2021-09-21 13:27:14 +0530

    spring boot

--- SpringBootAppDemo/src/main/java/com/ranadheer/springboot/springbootappdemo/controllers/RegisterController.java
+++ SpringBootAppDemo/src/main/java/com/ranadheer/springboot/springbootappdemo/controllers/RegisterController.java
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
 
+
+    @Autowired
     private MyUserDetailsService userDetailsService;
 
     @Autowired
